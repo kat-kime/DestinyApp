a4c11dc341dd5d1138fe6007c57e41eed3537399
kat-kime <katckime@gmail.com>
2020-08-03 19:34:13 -0700

    Adding new progress

--- app/src/main/java/com/katexcellence/destinyapp/NewHabit.java
+++ app/src/main/java/com/katexcellence/destinyapp/NewHabit.java
@@ -1,4 +1,21 @@
 package com.katexcellence.destinyapp;
 
-public class NewHabit {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewHabit extends AppCompatActivity {
+
+    // Declaring instance variables
+    EditText newHabitField;
+    Button saveNewHabit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_habit);
+    }
 }
