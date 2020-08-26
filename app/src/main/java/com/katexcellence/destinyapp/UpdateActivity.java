9ca946dd2f25278acf84de90a1b3e218f90d31b5
kat-kime <katckime@gmail.com>
2020-08-25 21:40:20 -0700

    Simplifying the user flow by merging the habits and add new habit page.
    (To be honest, I'm just not sure why the button listener on the new habit page isn't working, so this is the easiest fix until I become more knowledgable.)

--- app/src/main/java/com/katexcellence/destinyapp/UpdateActivity.java
+++ app/src/main/java/com/katexcellence/destinyapp/UpdateActivity.java
@@ -3,6 +3,7 @@ package com.katexcellence.destinyapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,7 @@ public class UpdateActivity extends AppCompatActivity {
     // Declaring instance variables
     RecyclerView habitList;
     Button newHabitButton;
+    EditText newHabitText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,14 +23,7 @@ public class UpdateActivity extends AppCompatActivity {
         // Assigning variables to items
         habitList = findViewById(R.id.habitList);
         newHabitButton = findViewById(R.id.newHabitButton);
-
-        // Adding a button listener
-        newHabitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setContentView(R.layout.activity_new_habit);
-            }
-        });
+        newHabitText = findViewById(R.id.newHabitText);
 
     }
 }
